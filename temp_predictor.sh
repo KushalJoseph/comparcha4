9540d794969ad192498090b18dd1e6b13c867b71
Kushal Joseph Vallamkatt <kjv5316@e5-cse-135-28.cse.psu.edu>
2024-11-07 23:56:57 -0500

    init

---
+++ temp_predictor.sh
@@ -0,0 +1,43 @@
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+--outdir=predictor/m5out_Bubblesort_large \
+--redirect-stdout \
+--redirect-stderr \
+cse530_a4_sys_config.py \
+--cse530-core-config=large \
+--binfile=benchmarks/stanford/Bubblesort \
+--l1i_size "4kB" \
+--l1d_size "4kB" \
+--l2_size "32kB"
+
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+--outdir=predictor/m5out_Bubblesort_hard \
+--redirect-stdout \
+--redirect-stderr \
+cse530_a4_sys_config.py \
+--cse530-core-config=hard \
+--binfile=benchmarks/stanford/Bubblesort \
+--l1i_size "4kB" \
+--l1d_size "4kB" \
+--l2_size "32kB"
+
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+--outdir=predictor/m5out_Bubblesort_medium \
+--redirect-stdout \
+--redirect-stderr \
+cse530_a4_sys_config.py \
+--cse530-core-config=medium \
+--binfile=benchmarks/stanford/Bubblesort \
+--l1i_size "4kB" \
+--l1d_size "4kB" \
+--l2_size "32kB"
+
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+--outdir=predictor/m5out_Bubblesort_soft \
+--redirect-stdout \
+--redirect-stderr \
+cse530_a4_sys_config.py \
+--cse530-core-config=soft \
+--binfile=benchmarks/stanford/Bubblesort \
+--l1i_size "4kB" \
+--l1d_size "4kB" \
+--l2_size "32kB"
---
+++ temp_robs.sh
@@ -0,0 +1,98 @@
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+--outdir=good/small \
+--redirect-stdout \
+--redirect-stderr \
+cse530_a4_sys_config.py \
+--cse530-core-config=small \
+--binfile=benchmarks/stanford/Bubblesort \
+--l1i_size "4kB" \
+--l1d_size "4kB" \
+--l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_ultrahard \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=ultrahard \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_superhard \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=superhard \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_hard \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=hard \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_medium \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=medium \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_soft \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=soft \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_supersoft \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=supersoft \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_ultrasoft \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=ultrasoft \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
+
+# /home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt \
+# --outdir=rob/m5out_Bubblesort_small \
+# --redirect-stdout \
+# --redirect-stderr \
+# cse530_a4_sys_config.py \
+# --cse530-core-config=small \
+# --binfile=benchmarks/stanford/Bubblesort \
+# --l1i_size "4kB" \
+# --l1d_size "4kB" \
+# --l2_size "32kB"
